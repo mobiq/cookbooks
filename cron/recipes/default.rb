77635556a1cb827d012ba2ad3a85be885cdae14f
Hiroshi Horie <private@hiroshihorie.com>
2015-02-11 01:29:43 +0700

    cb

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -4,5 +4,5 @@ cron "every_minute" do
   day "*"
   month "*"
   weekday "*"
-  command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everyminute.php"
+  command "cd /srv/www/mobiq_cron_api/current && php .lib/cron/cron-everyminute.php"
 end
