3ab6bb159e01100c297fdb7504243d2d0a555eeb
Hiroshi Horie <private@hiroshihorie.com>
2015-02-13 02:38:55 +0700

    new cron for 8

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -6,3 +6,13 @@ cron "every_minute" do
   weekday "*"
   command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everyminute.php"
 end
+
+# 8:00 GMT, 17:00 JST
+cron "everydayhour8" do
+  minute "0"
+  hour "8"
+  day "*"
+  month "*"
+  weekday "*"
+  command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everydayhour8.php"
+end
