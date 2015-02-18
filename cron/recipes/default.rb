7dcef8ac3d920f141345e282ca0b1f39f597393e
Hiroshi Horie <private@hiroshihorie.com>
2015-02-18 23:49:51 +0700

    cron everyhour

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -1,18 +1,28 @@
 cron "every_minute" do
-  minute "*"
-  hour "*"
-  day "*"
-  month "*"
-  weekday "*"
-  command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everyminute.php"
+    minute "*"
+    hour "*"
+    day "*"
+    month "*"
+    weekday "*"
+    command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everyminute.php"
+end
+
+cron "everyhour" do
+    minute "*"
+    hour "*"
+    day "*"
+    month "*"
+    weekday "*"
+    command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everyhour.php"
 end
 
 # 8:00 GMT, 17:00 JST
 cron "everydayhour8" do
-  minute "0"
-  hour "8"
-  day "*"
-  month "*"
-  weekday "*"
-  command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everydayhour8.php"
+    minute "0"
+    hour "8"
+    day "*"
+    month "*"
+    weekday "*"
+    command "cd /srv/www/mobiq_cron_api/current && php ./cron/cron-everydayhour8.php"
 end
+
